2946127d19d5753d9a6ef35a0946405f0b7f2eff
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-07-30 15:32:32 +0900

    init firebase

---
+++ utils/firebase/init.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDC6kF3el9LjB_zCNxw18_dJYEt2oHDm9o",
+  authDomain: "myapp-ca36f.firebaseapp.com",
+  projectId: "myapp-ca36f",
+  storageBucket: "myapp-ca36f.appspot.com",
+  messagingSenderId: "962225210889",
+  appId: "1:962225210889:web:aa605f89a376f0e1ca5ad8",
+  measurementId: "G-4SC4RHVKD9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
